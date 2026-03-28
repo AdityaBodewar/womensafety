9be15a4fc13098d3ca7e1b6f5e0e2d8851ec18e4
Aditya <adityabodewar835@gmail.com>
2026-03-29 05:12:53 +0530

    necessary installation done

--- Backend/run.py
+++ Backend/run.py
@@ -1,7 +1,10 @@
 from application import app
+import os
+from dotenv import load_dotenv
 
-
+load_dotenv()
+port=os.getenv("PORT")
 
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=port,)
